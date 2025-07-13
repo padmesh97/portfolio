f974a33b4b91c8067645f11f729d2594054332e5
padmesh97 <kunwarpadmesh@yahoo.com>
2025-07-13 16:19:01 +0530

    updated og image

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
   <!-- OG Tags -->
   <meta property="og:type" content="website" />
   <meta property="og:title" content="Padmesh K. - Full Stack Developer, DevOps Engineer & Tech Evangelist" />
-  <meta property="og:image" content="https://www.mozilla.org/media/img/firefox/home/fx-desktop.png" />
+  <meta property="og:image" content="https://www.padmeshkunwar.me/images/og_tile.png" />
   <meta property="og:url" content="https://www.padmeshkunwar.me/" />
   <meta property="og:site_name" content="padmeshkunwar.me" />
   <meta property="og:description" content="Discover my work in web development, AI, and cloud engineering." />
@@ -30,7 +30,7 @@
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="Padmesh K. - Full Stack Developer, DevOps Engineer & Tech Evangelist" />
   <meta name="twitter:description" content="Discover my work in web development, AI, and cloud engineering." />
-  <meta name="twitter:image" content="https://www.mozilla.org/media/img/firefox/home/fx-desktop.png" />
+  <meta name="twitter:image" content="https://www.padmeshkunwar.me/images/og_tile.png" />
 
   <!-- Fallback description Tags -->
   <meta name="description" content="Discover my work in web development, AI, and cloud engineering.">
