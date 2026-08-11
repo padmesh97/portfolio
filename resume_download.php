a41af9dc001512b851b65c0fe0218dd3caa46062
padmesh97 <kunwarpadmesh@yahoo.com>
2026-08-11 23:38:18 +0530

    gcp-pca redirection fixed

--- resume_download.php
+++ resume_download.php
@@ -9,4 +9,4 @@ if ($status == "success") {
     fwrite($fp, $text);
     fclose($fp);
 }
-header("location: ./files/padmesh_kunwar_v_12_10.pdf");
+header("location: ./files/padmesh_kunwar_v_13.pdf");
